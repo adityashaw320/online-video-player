c846dee938b229010312953f6da2e21ffd710830
Aditya shaw <silentadhi1234@gmail.com>
2025-09-06 19:16:19 +0530

    main

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,11 @@
-import { json } from "express";
+// import { json } from "express";
 import { asyncHandeller } from "../utils/asyncHandeller.js";
 
 const registerUser = asyncHandeller(async(req, res)=>{
-    res.status(200)+json({
-        message: "ok"
+   res.status(200).json({
+    message: "ok"
+   })
     })
-});
+
 
 export { registerUser }
