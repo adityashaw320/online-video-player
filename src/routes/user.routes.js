79d65fce93c82785328baa579be8b2b1b2d95d5d
Aditya shaw <silentadhi1234@gmail.com>
2025-09-12 15:59:45 +0530

    refactor: organize user routes and add additional endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refeshAccessToken } from "../controllers/user.controller.js";
+import {
+     loginUser,
+     logoutUser,
+     registerUser, 
+     refeshAccessToken,
+     changeCurrentUserPassword,
+     getCurrentUser, 
+     updateAccountDetails, 
+     updateUserAvatar, 
+     updateUserCoverImage, 
+     getuserChannelProfile, 
+     getWatchHistory
+     }
+     from "../controllers/user.controller.js";
+
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +38,12 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refeshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentUserPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("/cover-image"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getuserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router
